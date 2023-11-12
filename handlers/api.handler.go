4b9bdcd1c54794dd0dab097a57b2c1cd162a9e9e
resqiar <resqiageng@gmail.com>
2023-11-12 16:53:34 +0700

    remove unused code

--- handlers/api.handler.go
+++ handlers/api.handler.go
@@ -1,7 +1,6 @@
 package handlers
 
 import (
-	"log"
 	"sql-playground/services"
 	"strconv"
 
@@ -13,8 +12,8 @@ func InitAPIHandler(server *fiber.App) {
 
 	api.Get("/load-more-city", func(c *fiber.Ctx) error {
 		rawPage := c.Query("page", "1")
+
 		page, err := strconv.Atoi(rawPage)
-		log.Println(page)
 		if err != nil {
 			return c.JSON(fiber.Map{
 				"Error": err,
@@ -22,23 +21,6 @@ func InitAPIHandler(server *fiber.App) {
 		}
 
 		data := services.GetAllCities(page)
-
-		// var builder strings.Builder
-		//
-		// for _, v := range data {
-		// 	fmt.Fprintf(&builder, `
-		// 		<tr>
-		// 			<td>%d</td>
-		// 			<td>%s</td>
-		// 			<td>%s</td>
-		// 			<td>%s</td>
-		// 			<td>%d</td>
-		// 		</tr>
-		// 	`,
-		// 		v.ID, v.Name, v.CountryCode, v.District, v.Population,
-		// 	)
-		// }
-
 		return c.JSON(data)
 	})
 }
